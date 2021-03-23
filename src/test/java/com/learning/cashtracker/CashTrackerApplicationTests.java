0da52614a8d37d415b010525576a12ea52aaa008
Dawid <davis693@o2.pl>
2021-03-23 20:29:53 +0100

    initial commit

---
+++ src/test/java/com/learning/cashtracker/CashTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.cashtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
